b52fb479388e1253c1126bfc298a6d53c024bb7f
nengcsn <73385647+nengcsn@users.noreply.github.com>
2021-05-26 19:03:58 +0800

    new

--- Assets/script/StoneLineManager.cs
+++ Assets/script/StoneLineManager.cs
@@ -9,8 +9,12 @@ public class StoneLineManager : MonoBehaviour
     Stone[] Stones;
     public GameObject LineStart;
     public GameObject LineEnd;
+    public GameObject LineStart1;
+    public GameObject LineEnd1;
     public LineRenderer LineRenderer;
+    public LineRenderer LineRenderer1;
     private Vector3 _targetNormal;
+    private Vector3 _targetNormal2;
 
 
     void Start()
@@ -18,9 +22,13 @@ public class StoneLineManager : MonoBehaviour
         //Find all the stones in my project and create stone objects
         Stones = GameObject.FindGameObjectsWithTag("Stone").Select(s => new Stone(s)).ToArray();
         _targetNormal = LineEnd.transform.position - LineStart.transform.position;
+        _targetNormal2 = LineEnd1.transform.position - LineStart1.transform.position;
         LineRenderer.positionCount = 2;
         LineRenderer.SetPosition(0, LineStart.transform.position);
         LineRenderer.SetPosition(1, LineEnd.transform.position);
+        LineRenderer1.positionCount = 2;
+        LineRenderer1.SetPosition(0, LineStart1.transform.position);
+        LineRenderer1.SetPosition(1, LineEnd1.transform.position);
     }
 
     private void Update()
@@ -31,22 +39,36 @@ public class StoneLineManager : MonoBehaviour
             //Rotate stones according to the longest directions and voxelise them
             for (int i = 0; i < Stones.Length; i++)
             {
-                //Stones[i].PlaceStoneByLongestDirection();
-                Stones[i].VoxeliseMesh();//变网格
-                Stones[i].OrientNormal(_targetNormal);//旋转
-                Stones[i].MoveStartToPosition(targetPosition);
-                Debug.Log(Stones[i].NormalStart.transform.position);
-                Debug.Log(Stones[i].NormalEnd.transform.position);
-                targetPosition = Stones[i].NormalStart.transform.position;
+                if (i < Stones.Length / 2)
+                {
+
+                    //Stones[i].PlaceStoneByLongestDirection();
+                    Stones[i].VoxeliseMesh();
+                    Stones[i].OrientNormal(_targetNormal);
+                    Stones[i].MoveStartToPosition(targetPosition);
+                    targetPosition = Stones[i].NormalStart.transform.position;
+                    Debug.Log(Stones[i].NormalStart.transform.position);
+                    Debug.Log(Stones[i].NormalEnd.transform.position);
+                }
+                else
+                {
+                    targetPosition = LineStart1.transform.position;
+                    Stones[i].VoxeliseMesh();
+                    Stones[i].OrientNormal(_targetNormal);
+                    Stones[i].MoveStartToPosition(targetPosition);
+                    targetPosition = Stones[i].NormalStart.transform.position;
+                }
             }
         }
     }
 }
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
 
+
+//if(NormalStart.tranform.position-LineEnd.transform.position>(0,0,0))
+
+
+
+/*
 public class StoneLineManager : MonoBehaviour
 {
     //Voxelise the mesh
@@ -79,7 +101,7 @@ public class StoneLineManager : MonoBehaviour
                 Stones[i].VoxeliseMesh();
                 Stones[i].OrientNormal(_targetNormal);
                 Stones[i].MoveStartToPosition(targetPosition);
-                targetPosition = Stones[i].NormalEnd.transform.position;//修改为Normalstart 头尾相连
+                targetPosition = Stones[i].NormalEnd.transform.position;
             }
         }
     }
