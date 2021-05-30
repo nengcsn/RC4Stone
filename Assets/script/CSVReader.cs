8c413fc5a456ea36123eb778d8ed7d0cba27bbff
echoecho0716 <73584342+echoecho0716@users.noreply.github.com>
2021-05-30 22:14:32 +0800

    0530echo
    Start and End points read from text file

---
+++ Assets/script/CSVReader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public static class CSVReader
+{
+    
+    public static Vector3 ReadStartPoints(int pointNumber)
+    {
+
+        string fileContent = Resources.Load<TextAsset>("Data/start_points").text;
+
+        string[] lines = fileContent.Split('\n');
+        string[] allStartPointsX = lines.Select(l => l.Split(',')[0]).ToArray();
+        string[] allStartPointsY = lines.Select(l => l.Split(',')[1]).ToArray();
+        string[] allStartPointsZ = lines.Select(l => l.Split(',')[2]).ToArray();
+        float startX = float.Parse(allStartPointsX[pointNumber]);
+        float startY = float.Parse(allStartPointsY[pointNumber]);
+        float startZ = float.Parse(allStartPointsZ[pointNumber]);
+
+
+
+        var lineStart = GameObject.Find("pointstart");
+        lineStart.transform.position= new Vector3(startX, startY, startZ);
+        return lineStart.transform.position;
+
+
+        
+    }
+
+    public static Vector3 ReadEndPoints(int pointNumber)
+    {
+
+        string fileContent = Resources.Load<TextAsset>("Data/end_points").text;
+
+        string[] lines = fileContent.Split('\n');
+        string[] allEndPointsX = lines.Select(l => l.Split(',')[0]).ToArray();
+        string[] allEndPointsY = lines.Select(l => l.Split(',')[1]).ToArray();
+        string[] allEndPointsZ = lines.Select(l => l.Split(',')[2]).ToArray();
+        float endX = float.Parse(allEndPointsX[pointNumber]);
+        float endY = float.Parse(allEndPointsY[pointNumber]);
+        float endZ = float.Parse(allEndPointsZ[pointNumber]);
+
+
+
+        var lineEnd = GameObject.Find("pointend");
+        lineEnd.transform.position = new Vector3(endX, endY, endZ);
+        return lineEnd.transform.position;
+
+    }
+}
---
+++ Assets/script/EnvironmentManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnvironmentManager : MonoBehaviour
+{
+
+
+    //public GameObject prefabStart = Resources.Load<GameObject>("Prefabs/pointstart");
+    //public GameObject prefabEnd = Resources.Load<GameObject>("Prefabs/pointend");
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] lines = Resources.Load<TextAsset>("Data/start_points").text.Split('\n');
+        for (int i =0; i<lines.Length; i++)
+        {
+            var testStart = CSVReader.ReadStartPoints(i);
+            var testEnd = CSVReader.ReadEndPoints(i);
+
+            print("startpoint"+i+" is" + testStart);
+            print("endpoint" +i+ " is" + testEnd);
+
+
+
+            Instantiate(Resources.Load<GameObject>("Prefabs/pointstart"), testStart, Quaternion.identity);
+            Instantiate(Resources.Load<GameObject>("Prefabs/pointend"), testEnd, Quaternion.identity);
+        }
+        
+
+
+        /*
+        var test1 = CSVReader.ReadStartPoints(2);
+        print("startpoint is"+test1);
+        var test2 = CSVReader.ReadEndPoints(2);
+        print("endpoint is"+test2);
+        */
+
+    }
+}
