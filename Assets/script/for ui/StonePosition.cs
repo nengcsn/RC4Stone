97d91036b7b650d39aefb913a0d76eead4725a1a
echozhu0716 <73614933+echozhu0716@users.noreply.github.com>
2021-08-10 23:07:03 +0800

    Add UI Text

---
+++ Assets/script/for ui/StonePosition.cs
@@ -0,0 +1,24 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StonePosition : MonoBehaviour
+{
+    private Vector3 stonePosition = GameObject.Find("Stone_1").transform.position;
+    public Text positionText;
+
+    private void Update()
+    {
+        positionText.text = "Stone Position:" + stonePosition;
+
+        positionText = GameObject.Find("TextStone01").GetComponent<Text>();
+        positionText.transform.position = GameObject.Find("Stone_1").transform.position;
+
+
+
+
+}
+
+}
